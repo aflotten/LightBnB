10af9661fd2fca2aaf91b058e4054ff3039de7cc
Anders Flotten <anders3835@hotmail.com>
2022-12-15 02:51:41 +0000

    linting

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -200,11 +200,11 @@ const addProperty = function(property) {
     property.parking_spaces,
     property.number_of_bathrooms,
     property.number_of_bedrooms
-  ]
+  ];
 
   const queryString = `
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;
-  `
+  `;
   return pool
   .query(queryString, insertParams)
   .then(res => res.rows)
